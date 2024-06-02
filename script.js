8e4772c164b9b3e037a9ba40481297289711bc87
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-06-02 00:22:29 -0700

    Add basic math functions

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+function add(previousNumber, currentNumber) {
+  return previousNumber + currentNumber;
+}
+
+function subtract(previousNumber, currentNumber) {
+  return previousNumber - currentNumber;
+}
+
+function multiply(previousNumber, currentNumber) {
+  return previousNumber * currentNumber;
+}
+
+function divide(previousNumber, currentNumber) {
+  return previousNumber * currentNumber;
+}
